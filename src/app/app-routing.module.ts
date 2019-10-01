6fb40734eb3610753bc02060d6ff9d93a32a700e
Jordan Gray <jordan@quineweb.com>
2019-10-01 02:34:27 +0100

    Add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WordTestComponent } from './words/word-test/word-test.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: WordTestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
