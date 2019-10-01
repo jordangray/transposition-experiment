5f19c99908ccf070c4a5469cbba1dfa0b54ffd77
Jordan Gray <jordan@quineweb.com>
2019-10-01 03:14:25 +0100

    Register form initial implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WordTestComponent } from './words/word-test/word-test.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
+  { path: '', component: RegisterComponent },
   { path: 'test', component: WordTestComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WordsModule } from './words/words.module';
+import { RegisterModule } from './register/register.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { WordsModule } from './words/words.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WordsModule
+    WordsModule,
+    RegisterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
