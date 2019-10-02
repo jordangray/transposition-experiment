e8b1dc557e2b5dd5c49779fa0742e8a70961df5f
Jordan Gray <jordan@quineweb.com>
2019-10-02 21:25:48 +0100

    Use Angular deploy plugin

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 # Ensure branch is up-to-date
 git checkout master
 
-# Build project
+# Build and deploy
 npm install
-ng build --prod --output-path=dist --base-href /transposition-experiment/
-
-# Push to GitHub Pages branch
-git worktree add dist gh-pages
-(cd dist; git add .)
-(cd dist; git commit -m "Build output as of $(git log '--format=format:%H' master -1)")
-git push origin gh-pages
+ng deploy --base-href=/transposition-experiment/
