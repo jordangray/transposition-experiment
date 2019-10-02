a99922d291287854ae00cf790958047e017e5ecc
Jordan Gray <jordan@quineweb.com>
2019-10-02 20:50:12 +0100

    New build approach (without dist folder)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
+# Ensure branch is up-to-date
 git checkout master
+
+# Build project
 npm install
 ng build --prod --output-path=dist --base-href /transposition-experiment/
-git subtree split --prefix dist -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
+
+# Push to GitHub Pages branch
+git worktree add dist gh-pages
+(cd dist; git add .)
+(cd dist; git commit -m "Build output as of $(git log '--format=format:%H' master -1)")
+git push origin gh-pages
