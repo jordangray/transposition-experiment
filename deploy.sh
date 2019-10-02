c1b3d29f8d1706b035cbfeac10c4f9e544caac03
Jordan Gray <jordan@quineweb.com>
2019-10-02 20:40:03 +0100

    Fixed issue with deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+git checkout master
 npm install
 ng build --prod --output-path=dist --base-href /transposition-experiment/
-git push origin `git subtree split --prefix dist master`:gh-pages --force
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
